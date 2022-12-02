d07f75302d52dfdab8d0aca301245e8e6292fbf3
Haim Agiv <95864976+Haimagiv28@users.noreply.github.com>
2022-12-02 15:23:59 +0200

    Add files via upload

---
+++ Backend/main.py
@@ -0,0 +1,42 @@
+import json
+from fastapi import FastAPI
+from pydantic import BaseModel
+from models import *
+
+
+class numbers(BaseModel):
+    num1: float
+    num2: float
+
+
+app = FastAPI()
+
+
+@app.get("/")
+def welcome():
+    return "Welcome to Haim's Calculator"
+
+
+@app.post("/add")
+def add_calc(numberTemp: numbers):
+    return {"The sum is":  add(numberTemp.num1, numberTemp.num2)}
+
+
+@app.post("/sub")
+def sub_calc(numberTemp: numbers):
+    return {"The sum is":  sub(numberTemp.num1, numberTemp.num2)}
+
+
+@app.post("/multy")
+def multy_calc(numberTemp: numbers):
+    return {"The sum is":  multy(numberTemp.num1, numberTemp.num2)}
+
+
+@app.post("/divide")
+def divide_calc(numberTemp: numbers):
+    return {"The sum is":  divide(numberTemp.num1, numberTemp.num2)}
+
+
+@app.post("/pow")
+def pow_calc(numberTemp: numbers):
+    return {"The sum is":  power(numberTemp.num1, numberTemp.num2)}
---
+++ Backend/models.py
@@ -0,0 +1,20 @@
+import math
+
+def add(a, b):
+    return a+b
+
+
+def sub(a, b):
+    return a-b
+
+
+def multy(a, b):
+    return a*b
+
+
+def divide(a, b):
+    return a/b
+
+
+def power(a, b):
+    return pow(a, b)
