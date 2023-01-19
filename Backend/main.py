86b39cff11670adea2207c3203eb2f3c087715f7
Haimagiv28 <haimagiv28@gmail.com>
2023-01-19 21:50:38 +0200

    haim

--- Backend/main.py
+++ Backend/main.py
@@ -20,29 +20,34 @@ def welcome():
 
 @app.post("/add")
 def add_calc(numberBase: numbers):
-    return {"Res":  add(numberBase.num1, numberBase.num2)}
+    return {add(numberBase.num1, numberBase.num2)}
 
 
 @app.post("/sub")
 def sub_calc(numberBase: numbers):
-    return {"Res":  sub(numberBase.num1, numberBase.num2)}
+    return {sub(numberBase.num1, numberBase.num2)}
 
 
 @app.post("/multy")
 def multy_calc(numberBase: numbers):
-    return {"Res":  multy(numberBase.num1, numberBase.num2)}
+    return {multy(numberBase.num1, numberBase.num2)}
 
 
 @app.post("/divide")
 def divide_calc(numberBase: numbers):
-    return {"Res":  divide(numberBase.num1, numberBase.num2)}
+    return {divide(numberBase.num1, numberBase.num2)}
 
 
 @app.post("/pow")
 def pow_calc(numberBase: numbers):
-    return {"Res:":  power(numberBase.num1, numberBase.num2)}
+    return {power(numberBase.num1, numberBase.num2)}
 
 
 @app.post("/linear_plot")
 def linear_plot_calc(numberBase: numbers):
-    return {"Res:": linear_plot(numberBase.num1, numberBase.num2)}
+    return {"Res": linear_plot(numberBase.num1, numberBase.num2)}
+
+
+@app.post("/percent")
+def percentXofY_calc(numberBase: numbers):
+    return {percent(numberBase.num1, numberBase.num2)}
--- Backend/models.py
+++ Backend/models.py
@@ -1,34 +1,45 @@
-import math
 import numpy as np
+import json
 
 
 def add(a, b):
-    return a+b
+    return ("{:.3f}".format(a+b))
 
 
 def sub(a, b):
-    return a-b
+    return ("{:.3f}".format(a-b))
 
 
 def multy(a, b):
-    return a*b
+    return ("{:.3f}".format(a*b))
 
 
 def divide(a, b):
-    return a/b
+    return ("{:.3f}".format(a/b))
 
 
 def power(a, b):
-    return pow(a, b)
+    return ("{:.3f}".format((a**b)))
 
 
 def linear_plot(a, b):
     # y = a*x+b
-
-    x = np.linspace(-2, 2, 1)
+    min_range = np.random.uniform(-10, 10)
+    # the range will be between -10 to 10
+    max_range = np.random.uniform(-10, 10)
+    if min_range > max_range:
+        # make sure the min is smaller than the max
+        min_range, max_range = max_range, min_range
+    x = np.random.uniform(min_range, max_range, 10)
     y = a*x + b
     res = {
         "x": x.tolist(),
         "y": y.tolist()
     }
     return res
+
+
+def percent(a, b):  # 20 , 10  # the percent of x from y
+    x = b  # 20
+    y = x * (a*0.01)
+    return ("{:.2f}".format(y))
